8d36a51b9d50e94912051fe8973418eda0902273
samkitkat <samarbarakat23@gmail.com>
2020-07-13 10:44:36 -0700

    Updates

---
+++ precache-manifest.41b1b71ede814530c2f5ac4e09d658e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5075d1a0340159295da3a92c3a02970a",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "08eafa83f3e25c768d1d",
+    "url": "/react/static/css/main.965f0e3e.chunk.css"
+  },
+  {
+    "revision": "db04ff3a5e19d370a1b1",
+    "url": "/react/static/js/2.5ecea5ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.5ecea5ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08eafa83f3e25c768d1d",
+    "url": "/react/static/js/main.d026f106.chunk.js"
+  },
+  {
+    "revision": "9bd9648bdc54eef630c0",
+    "url": "/react/static/js/runtime-main.d367309e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-app/precache-manifest.d9525e53e07bec064c05987c29ee8cee.js"
+  "/react/precache-manifest.41b1b71ede814530c2f5ac4e09d658e0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
